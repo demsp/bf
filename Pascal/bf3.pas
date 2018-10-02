19e0fad07673fae4665f8536cf06ca99d8380fc2
Dima <dserp18@gmail.com>
2018-10-02 23:50:24 +0300

    Create bf3.pas

---
+++ Pascal/bf3.pas
@@ -0,0 +1,38 @@
+LABEL prev,next;
+var
+ data_arr:array[1..10] of integer;    // массив данных
+ str_arr: string;                     // команды  
+ i,j,k: integer;                       // индексы строки и массива
+ i_stor: integer; 
+begin
+ j:=1;                  // нумерация элементов массива (внезапно) начинается с единицы
+ i:=1;
+ readln(str_arr);       //считываем строку
+ prev:
+ if i>length(str_arr) then goto next; 
+    if (str_arr[i]='+') then data_arr[j]:= data_arr[j]+1;
+    if (str_arr[i]='-') then data_arr[j]:= data_arr[j]-1;
+    if (str_arr[i]='>') then j:=j+1;
+    if (str_arr[i]='<') then j:=j-1;
+    if (str_arr[i]='.') then write(chr(data_arr[j]));
+    if (str_arr[i]='[') then
+     begin  
+      if data_arr[j]>0 then i_stor:=i;
+     end;
+    if (str_arr[i]=']') then
+     begin  
+      if data_arr[j]>0 then 
+       begin
+       i:=i_stor;
+       goto prev;
+       end;
+     end;
+    
+ i:=i+1;
+ goto prev;
+ next:
+for k:=1 to 10 do begin 
+write(data_arr[k]);
+write(' ');
+end;
+end.
