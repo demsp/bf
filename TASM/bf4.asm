834f69cebea10d8e839c983ee8904a4b4d1f4d63
Dima <dserp18@gmail.com>
2018-10-03 18:08:45 +0300

    Update bf4.asm

--- TASM/bf4.asm
+++ TASM/bf4.asm
@@ -12,54 +12,50 @@ prev:
  mov BL, j           ; загружаем в BL индекс данных
  inc data_arr[BX]    ; да, увеличиваем  значение в ячейке на 1
 next: 
- ;;;
  cmp DL, 2Dh         ; ячейка содержит -
  jne next1           ; нет, переходим на метку next1  
  mov BL, j 
  dec data_arr[BX]    ;BX, но не Bl 
 next1: 
- cmp DL, 3Eh        ; ячейка содержит >
- jne next2          ; нет, переходим на метку next2  
- inc j              ; да, переходим на сдедующий элемент массива data_arr
+ cmp DL, 3Eh         ; ячейка содержит >
+ jne next2           ; нет, переходим на метку next2  
+ inc j               ; да, переходим на сдедующий элемент массива data_arr
 next2: 
- cmp DL, 3Ch        ; ячейка содержит <
- jne next3          ; нет, переходим на метку next3  
- dec j              ; да, переходим на предыдущий элемент массива data_arr
+ cmp DL, 3Ch         ; ячейка содержит <
+ jne next3           ; нет, переходим на метку next3  
+ dec j               ; да, переходим на предыдущий элемент массива data_arr
 next3: 
- cmp DL, 2Eh        ; ячейка содержит .
- jne next4          ; нет, переходим на метку next4  
- mov AH,2           ; да, выводим содержимое ячейки
+ cmp DL, 2Eh         ; ячейка содержит .
+ jne next4           ; нет, переходим на метку next4  
+ mov AH,2            ; да, выводим содержимое ячейки
  mov BL, j
  mov DL, data_arr[BX]
  int 21h
- next4:
- cmp DL, 5Bh        ; ячейка содержит [
- jne next5          ; нет, переходим на метку next5
+next4:
+ cmp DL, 5Bh         ; ячейка содержит [
+ jne next5           ; нет, переходим на метку next5
  mov BL, j
  mov DL, data_arr[BX]
- cmp DL, 00         ; да, проверяем текущий элемент data_arr на ноль  
- jz next5           ; если ноль, прыгаем дальше
- mov DL, i          ; если не ноль, то загружаем 
- mov i_stor, Dl     ; в i_stor значение переменнной i 
- ;;;
- next5:
- cmp DL, 5Dh       ; ячейка содержит ]
- jne next6         ; нет, переходим на метку next6
+ cmp DL, 00          ; да, проверяем текущий элемент data_arr на ноль  
+ jz next5            ; если ноль, прыгаем дальше
+ mov DL, i           ; иначе загружаем 
+ mov i_stor, Dl      ; в i_stor значение переменной i 
+next5:
+ cmp DL, 5Dh         ; ячейка содержит ]
+ jne next6           ; нет, переходим на метку next6
  mov BL, j
  mov DL, data_arr[BX]
- cmp DL, 00         ; да, проверяем текущий элемент data_arr на ноль  
- jz next5           ; если ноль, прыгаем дальше
- mov DL, i_stor          ; если не ноль, то загружаем 
- mov i, Dl     ; в i_stor значение переменнной i 
- ;jmp prev
- ;;;
- next6:
- inc i             ; переходим на следующий символ массива команд
+ cmp DL, 00          ; да, проверяем текущий элемент data_arr на ноль  
+ jz next5            ; если ноль, прыгаем дальше
+ mov DL, i_stor      ; иначе загружаем 
+ mov i, Dl           ; в i_stor значение переменной i 
+next6:
+ inc i               ; переходим к следующей команде
  mov BL, i
  mov DL, str_arr[BX]   
- loop prev         ; прыгаем на метку prev:
+ loop prev          ; прыгаем на метку prev:
          
- mov AX, 4c00h        ; завершение программы  
+ mov AX, 4c00h      ; завершение программы  
  int 21h 
 text ends
 
