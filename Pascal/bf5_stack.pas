1017c6f1daf300f6c4fd25cc09738b88107a16d8
Dima <dserp18@gmail.com>
2018-10-08 15:50:28 +0300

    Update bf5_stack.pas

--- Pascal/bf5_stack.pas
+++ Pascal/bf5_stack.pas
@@ -1,67 +1,93 @@
+type
+  //Указатель на элемент стека.
+  TPElem = ^TElem;
+  //Элемент стека.
+  TElem = record
+    Data : Integer;
+    PNext : TPElem;
+  end;
+ 
+//Добавление элемента на вершину стека.
+procedure StackPush(var aPStack, aPElem : TPElem);
 begin
- j:=1;                  // нумерация элементов массива (внезапно) начинается с единицы
- ii:=1;
+  if aPElem = nil then Exit;
+  aPElem^.PNext := aPStack;
+  aPStack := aPElem
+end;
+ 
+//Изъятие элемента с вершины стека.
+function StackPop(var aPStack, aPElem : TPElem) : Boolean;
+begin
+  Result := False;
+  if aPStack = nil then Exit;
+  aPElem := aPStack;
+  aPStack := aPElem^.PNext;
+  Result := True;
+end;
+ 
+//Удаление стека из памяти (очистка стека).
+procedure StackFree(var aPStack : TPElem);
+var
+  PDel : TPElem;
+begin
+  while aPStack <> nil do begin
+    PDel := aPStack;
+    aPStack := aPStack^.PNext;
+    Dispose(PDel);
+  end;
+end;
+///
+LABEL prev,next;
+
+var
+  PSt1, PElem : TPElem;
+ data_arr:array[1..10] of integer;    // массив данных
+ str_arr: string;                     // команды  
+ i,j,k: integer;                      // индексы строки и массива
+ i_stor: integer; 
+
+begin
+ j:=1;                  // нумерация элементов массива начинается с единицы
+ i:=1;
  //readln(str_arr);       //считываем строку
- str_arr:='+++[>+++[>+<-]<-]'; // 3*3 = 9
+  
+ str_arr:='+++[>+++[>+++[>+<-]<-]<-]'; // 3^3=27;
  PSt1 := nil;
  prev:
- if ii>length(str_arr) then goto next; 
-    if (str_arr[ii]='+') then data_arr[j]:= data_arr[j]+1;
-    if (str_arr[ii]='-') then data_arr[j]:= data_arr[j]-1;
-    if (str_arr[ii]='>') then j:=j+1;
-    if (str_arr[ii]='<') then j:=j-1;
-    if (str_arr[ii]='.') then write(chr(data_arr[j]));
+ if i>length(str_arr) then goto next; 
+    if (str_arr[i]='+') then data_arr[j]:= data_arr[j]+1;
+    if (str_arr[i]='-') then data_arr[j]:= data_arr[j]-1;
+    if (str_arr[i]='>') then j:=j+1;
+    if (str_arr[i]='<') then j:=j-1;
+    if (str_arr[i]='.') then write(chr(data_arr[j]));
     // скобки
-    {
-    if (str_arr[ii]='[') and (data_arr[j]>0) then
-     begin  
-      New(PElem);
-      PElem^.Data := ii;
-      StackPush(PSt1, PElem);
-      ii := ii+1;
-      goto prev;
-     end;
-     }
-     if (str_arr[ii]='[') then 
+    if (str_arr[i]='[') then 
       begin
       New(PElem);
-      PElem^.Data := ii;
+      PElem^.Data := i;
       StackPush(PSt1, PElem);
       if (data_arr[j]>0) then 
        begin
-       ii := ii+1;
+       i := i+1;
        goto prev;
        end;
       end;
-     
-    {
-    if (str_arr[ii]=']') and (data_arr[j]>0) then
-     begin  
-       //ii:=i_stor;
-       StackPop(PSt1, PElem); 
-       ii := PElem^.Data;
-       ii := ii+1;
-       goto prev;
-       end;
-     }
-     
-     if (str_arr[ii]=']') then
+    if (str_arr[i]=']') then
       begin
       StackPop(PSt1, PElem); 
       if (data_arr[j]>0) then 
        begin
-        ii := PElem^.Data;
-        //ii := ii+1;
+        i := PElem^.Data;
+        //i := i+1;
         goto prev;
        end;
       end;
      
-     
-    
- ii:=ii+1;
+ i:=i+1;
  goto prev;
  next:
 for k:=1 to 10 do begin 
 write(data_arr[k]);
 write(' ');
 end;
+end.
